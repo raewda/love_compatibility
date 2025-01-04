a2e5f8960339a9f56eb734c6c26ec7085b6088f2
wda <vorobieva2005dasha@gmail.com>
2025-01-04 22:24:22 +0700

    background image

--- app/src/main/java/com/example/love_compatibility/MainActivity.kt
+++ app/src/main/java/com/example/love_compatibility/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -17,11 +20,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.love_compatibility.ui.theme.Love_compatibilityTheme
+import com.example.love_compatibility.ui.theme.lighteggplant
 import com.example.love_compatibility.ui.theme.marckscript
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,90 +40,102 @@ class MainActivity : ComponentActivity() {
             val calculate = remember { mutableStateOf(false) }
             val she = remember { mutableStateOf(" ") }
             val he = remember { mutableStateOf(" ") }
+            val rezult = remember { mutableStateOf("") }
 
             Love_compatibilityTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .padding(innerPadding)
-                    ) {
-                        Text(text = "hi")
-
-                        Text(text = "hi hi hi")
-
-                        //textfield she
-                        OutlinedTextField(
-                            value = she.value,
-                            onValueChange = {
-                                new -> she.value = new
-                            },
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()) { innerPadding ->
+                    Box{
+                        Image(painter = painterResource(R.drawable.back_hearts), contentDescription = "background",
                             modifier = Modifier
-                                .padding(top = 10.dp, start = 15.dp, end = 20.dp),
-                            colors = TextFieldDefaults.colors(cursorColor = Color.Magenta),
-                            enabled = true,
-                            readOnly = false,
-                            placeholder = { Text("her name",
-                                color = Color.Magenta,
-                                fontFamily = marckscript,
-                                fontSize = 40.sp,) },
-                            textStyle = TextStyle(
-                                fontFamily = marckscript,
-                                fontSize = 40.sp,
-                                color = Color.Magenta
-                            ),
-                            maxLines = 1,
-                            shape = CircleShape
-                        )
-
-
-                        //textfield he
-                        OutlinedTextField(
-                            value = he.value,
-                            onValueChange = {
-                                    new -> he.value = new
-                            },
+                                .fillMaxSize(),
+                            contentScale = ContentScale.Crop)
+                        Column(
                             modifier = Modifier
-                                .padding(top = 10.dp, start = 15.dp, end = 20.dp),
-                            colors = TextFieldDefaults.colors(cursorColor = Color.Magenta),
-                            enabled = true,
-                            readOnly = false,
-                            placeholder = { Text("his name",
-                                color = Color.Magenta,
-                                fontFamily = marckscript,
-                                fontSize = 40.sp,) },
-                            textStyle = TextStyle(
-                                fontFamily = marckscript,
-                                fontSize = 40.sp,
-                                color = Color.Magenta
-                            ),
-                            maxLines = 1,
-                            shape = CircleShape
-                        )
-
-
-                        //calculate
-                        Button(
-                            onClick = {
-                                calculate.value = true
-                            },
-                            modifier = Modifier
+                                .padding(innerPadding)
                         ) {
-                            Text(text = "calculate")
-                        } //calculate
+                            Text(text = "hi")
 
-                        Text(text = she.value + "+" + he.value)
+                            Text(text = "hi hi hi")
 
-                        //again
-                        Button(
-                            onClick = {
-                                calculate.value = false
-                                she.value = " "
-                                he.value = " "
-                            }
-                        ) {
-                            Text(text = "Again")
-                        } //again
+                            //textfield she
+                            OutlinedTextField(
+                                value = she.value,
+                                onValueChange = {
+                                        new -> she.value = new
+                                },
+                                modifier = Modifier
+                                    .padding(top = 10.dp, start = 15.dp, end = 20.dp),
+                                colors = TextFieldDefaults.colors(cursorColor = lighteggplant),
+                                enabled = true,
+                                readOnly = false,
+                                placeholder = { Text("her name",
+                                    color = Color.Magenta,
+                                    fontFamily = marckscript,
+                                    fontSize = 40.sp,) },
+                                textStyle = TextStyle(
+                                    fontFamily = marckscript,
+                                    fontSize = 40.sp,
+                                    color = Color.Magenta
+                                ),
+                                maxLines = 1,
+                                shape = CircleShape
+                            )
+
+
+                            //textfield he
+                            OutlinedTextField(
+                                value = he.value,
+                                onValueChange = {
+                                        new -> he.value = new
+                                },
+                                modifier = Modifier
+                                    .padding(top = 10.dp, start = 15.dp, end = 20.dp),
+                                colors = TextFieldDefaults.colors(cursorColor = lighteggplant),
+                                enabled = true,
+                                readOnly = false,
+                                placeholder = { Text("his name",
+                                    color = Color.Magenta,
+                                    fontFamily = marckscript,
+                                    fontSize = 40.sp,) },
+                                textStyle = TextStyle(
+                                    fontFamily = marckscript,
+                                    fontSize = 40.sp,
+                                    color = Color.Magenta
+                                ),
+                                maxLines = 1,
+                                shape = CircleShape
+                            )
+
+
+                            //calculate
+                            Button(
+                                onClick = {
+                                    calculate.value = true
+                                    rezult.value = Random.nextInt(0,100).toString()
+                                },
+                                modifier = Modifier
+                            ) {
+                                Text(text = "calculate")
+                            } //calculate
+
+                            Text(text = she.value + " +" + he.value + " = " + rezult.value + "%")
+
+                            //again
+                            Button(
+                                onClick = {
+                                    calculate.value = false
+                                    she.value = " "
+                                    he.value = " "
+                                    rezult.value = ""
+                                }
+                            ) {
+                                Text(text = "again")
+                            } //again
+                        }
                     }
+
                 }
             }
         }
